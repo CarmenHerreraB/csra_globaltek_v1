310bacd5ea794573f5e40b408679974bf2bcfe58
CarmenHerreraB <carmen.herrera@globalteksecurity.com>
2025-04-03 09:55:33 -0500

    cambio modelo de activo

--- backend/backend1/settings.py
+++ backend/backend1/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'backend1.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "pruebas_db_csra_globaltek-prueba2",    #"db_csra_globaltek1",
+        "NAME": "pruebas2",    #"db_csra_globaltek1",
         "USER": "postgres",
         "PASSWORD": "19961994",
         "HOST": "localhost",
