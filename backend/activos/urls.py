7d7f589401f13a14241f9e8a9b4277baea6b6f4b
CarmenHerreraB <carmen.herrera@globalteksecurity.com>
2025-04-11 11:49:20 -0500

    valiacion NULL en activos

--- backend/activos/urls.py
+++ backend/activos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter # crear las rutas del crud
 from .apis.views.apiCrud import ActivoViewSet
 from .apis.views import ConfidencialidadViewSet,criticidadViewSet,IntegridadViewSet,DisponibilidadViewSet,CustodioViewset,DatospersonalesActivoViewset,TipodeactivoViewset,ProcesoViewset,DuenodeactivoViewset,EstadoxactivoViewset
-from .apis.views import ShowActivoValuesView
+from .apis.views import ShowActivoValuesView, ListAllActivosValuesView
 # 1. Crear el router
 router= DefaultRouter()
 # 2. Registrar el ViewSet en el router
@@ -21,5 +21,6 @@ router.register(r'Estadoxactivo', EstadoxactivoViewset, basename='Estadoxactivo'
 
 urlpatterns = [
     path('',include(router.urls) ),   #enrrutamiento
-    path('ActivoValues/<int:id>/', ShowActivoValuesView.as_view(), name='activo-values')
+    path('ActivoValues/<int:id>/', ShowActivoValuesView.as_view(), name='activo-values'),
+    path('allActivosValues/', ListAllActivosValuesView.as_view(), name='allActivos-values')
 ]
