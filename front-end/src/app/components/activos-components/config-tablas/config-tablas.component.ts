96e567dcc2760d4af2c383539f2255301c8c0435
mariahastamorir <maria.hastamorir@globalteksecurity.com>
2025-04-22 17:20:11 -0500

    Configuracion tablas

--- front-end/src/app/components/activos-components/config-tablas/config-tablas.component.ts
+++ front-end/src/app/components/activos-components/config-tablas/config-tablas.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivosService } from '../../../services/activos.service';
 
 @Component({
   selector: 'app-config-tablas',
@@ -6,6 +7,61 @@ import { Component } from '@angular/core';
   templateUrl: './config-tablas.component.html',
   styleUrl: './config-tablas.component.css'
 })
-export class ConfigTablasComponent {
+export class ConfigTablasComponent implements OnInit{
+
+  constructor(private activosService: ActivosService){}
+
+  confidencialidad: any[] = [];
+  integridad: any[] = [];
+  disponibilidad: any[] = [];
+  proceso: any[] = [];
+  tipodeactivo: any[] = [];
+  datospersonales: any[] = [];
+  custodio: any[] = [];
+  duenodeactivo: any[] = [];
+
+  
+
+  ngOnInit(): void {
+    //Cargar datos de confidencialidad
+    this.activosService.getConfidencialidad().subscribe(data => { 
+      this.confidencialidad = data;
+    });
+    
+    //Cargar datos de Integridad
+    this.activosService.getIntegridad().subscribe(data => { 
+      this.integridad = data;
+    });
+
+    //Cargar datos de Disponibilidad
+    this.activosService.getDisponibilidad().subscribe(data => { 
+      this.disponibilidad = data;
+    });
+
+    //Cargar datos de proceso
+    this.activosService.getProceso().subscribe(data => { 
+      this.proceso = data;
+    });
+
+    //Cargar datos de Tipo de activo
+    this.activosService.getTipoDeActivo().subscribe(data => { 
+      this.tipodeactivo = data;
+    });
+
+    //Cargar datos de datos personales activo
+    this.activosService.getDatosPersonalesActivo().subscribe(data => { 
+      this.datospersonales = data;
+    });
+
+    //Cargar datos de dueno de activo
+    this.activosService.getDuenoDelActivo().subscribe(data => { 
+      this.duenodeactivo = data;
+    });
+
+    //Cargar datos de custodio
+    this.activosService.getCustodio().subscribe(data => { 
+      this.custodio = data;
+    });
+  }
 
 }
