2d45065d6bb7fea36eb196b4b7bff5ef2cdba956
CarmenHerreraB <carmen.herrera@globalteksecurity.com>
2025-05-02 19:58:09 -0500

    solucion calcula valores

--- backend/activos/apis/views/formulas_calculator_View.py
+++ backend/activos/apis/views/formulas_calculator_View.py
@@ -2,18 +2,72 @@ from rest_framework import viewsets , status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from activos.apis.serializers.formulas_calculator_serializer import FormulasCalculatorSerializer
-from database.models.formulasCalculator import FormulasCalculator
+from database.models import Estadoxactivo,FormulasCalculator,Activo,Criticidad  
+
 
 class FormulasCalculatorViewset(viewsets.ModelViewSet):
     queryset= FormulasCalculator.objects.all().order_by('id') # traer modelo y ordenarlo por id
     serializer_class= FormulasCalculatorSerializer  # trate serializer
     
-    def destroy (self, request, *args, **kwards):
+    def validarValorCriticidad(self, activo):
+        estado=activo.estadoxactivo
+        if not estado:
+            return
+        
+        #Obtener formula activa o por default
+        formula_obj= FormulasCalculator.objects.filter(estado='activo').first()
+        if not formula_obj:
+            formula_obj= FormulasCalculator.objects.filter(default=True).first()
+        if not formula_obj:
+            print("No existe fórmula activa ni por defecto")
+            return 
+        if not formula_obj.formula:
+            print("La fórmula está vacía")
+            return
+        
+         # declarar variables de contexto con los valores de las tablas estado por activo
+        contexto = {
+                    'conf': estado.confidencialidad.valor if estado.confidencialidad and estado.confidencialidad.valor else 0,
+                    'intg': estado.integridad.valor if estado.integridad and estado.integridad.valor else 0,
+                    'disp': estado.disponibilidad.valor if estado.disponibilidad and estado.disponibilidad.valor else 0,}
+        
+        #evaluar la formula
+        try:
+            total_valor= eval(formula_obj.formula ,{}, contexto)
+            
+        except Exception as e:
+            print(f"error al implementar formula: {e}")
+            total_valor= sum(contexto.values()) # Fallback simple
+            
+        
+        #asignar valor a activos
+        
+        activo.valor=total_valor   
+        activo.save()
+        
+        
+        
+        #validar criticidad
+        criticidad_id = 3 if total_valor <= 5 else 2 if total_valor <= 10 else 1
+            
+        #traer modelo de criticidad para validar
+        try:
+            criticidad_obj= Criticidad.objects.get(id=criticidad_id)
+            estado.criticidad=criticidad_obj
+            estado.save()
+        except Criticidad.DoesNotExist:
+             print(f"Criticidad con ID {criticidad_id} no encontrada")
+        
+    
+    
+    
+    def destroy (self, request, *args, **kwargs):
         formula =self.get_object()
         if formula.default:
-            return Response({'Error:' 'Cannot delete the default formula'}, status=status.HTTP_403_FORBIDDEN)
+            return Response({'error': 'Cannot delete the default formula'}, status=status.HTTP_403_FORBIDDEN)
+
         else:
-            super().destroy(request, *args, **kwards)
+            self.perform_destroy(formula)
             return Response({'Response': 'Successfully deleted formula'}, status=status.HTTP_200_OK)
     
     @action(detail=True, methods=['post'], url_path='activar')
@@ -22,7 +76,13 @@ class FormulasCalculatorViewset(viewsets.ModelViewSet):
         formula=self.get_object()
         formula.estado='activo'
         formula.save()
+        
+         #invocar funcion  de calcular valor (validarValorCriticidad)y Recalcular todos los activos  
+        for activo in Activo.objects.all():
+            self.validarValorCriticidad(activo)
+        
         return Response ({'Response': 'Successfully activated formula'}, status=status.HTTP_200_OK)
+    
     
     @action(detail=False, methods=['get'], url_path='listar')
     def listar(self,request):
