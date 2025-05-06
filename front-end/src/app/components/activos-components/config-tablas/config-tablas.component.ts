640ea370876a66100c780b931c83c9d9c412b961
mariahastamorir <maria.hastamorir@globalteksecurity.com>
2025-05-06 11:46:33 -0500

    Arreglo api tablas sencillas con configuracion

--- front-end/src/app/components/activos-components/config-tablas/config-tablas.component.ts
+++ front-end/src/app/components/activos-components/config-tablas/config-tablas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivosService } from '../../../services/activos.service';
+import { ConfigTablasService } from '../../../services/config-tablas.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-config-tablas',
@@ -9,7 +11,7 @@ import { ActivosService } from '../../../services/activos.service';
 })
 export class ConfigTablasComponent implements OnInit{
 
-  constructor(private activosService: ActivosService){}
+  constructor(private activosService: ActivosService, private cofigTablasServices: ConfigTablasService){}
 
   confidencialidad: any[] = [];
   integridad: any[] = [];
@@ -22,7 +24,13 @@ export class ConfigTablasComponent implements OnInit{
 
   criticidad: any[] = [];
 
-  
+  //Inicializacion nuevos datos en las tablas
+  nuevoTipoActivo: string = '';
+  nuevoProceso: string = '';
+  nuevoCustodio: string = '';
+  nuevoDatosPersonales: string = '';
+  nuevoDuenoActivo: string = '';
+
 
   ngOnInit(): void {
     //Cargar datos de confidencialidad
@@ -71,4 +79,213 @@ export class ConfigTablasComponent implements OnInit{
     })
   }
 
+  //Agregar nuevo dato en tipo de activos 
+  agregarNuevoTipoActivo() {
+    if (this.nuevoTipoActivo.trim()) {
+      const data = { nombre: this.nuevoTipoActivo };
+      this.cofigTablasServices.agregarTipoActivo(data).subscribe({
+        next: (respuesta) => {
+          console.log('Tipo de activo agregado:', respuesta);
+          this.tipodeactivo.push(respuesta); // actualizar la tabla
+          this.nuevoTipoActivo = ''; // limpiar el input
+        },
+        error: (error) => {
+          console.error('Error al agregar tipo de activo:', error);
+        }
+      });
+    }
+  }
+
+  //Eliminar datos de tipo de activos 
+  eliminar(item: any) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `Se eliminará el tipo de activo "${item.nombre}"`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      confirmButtonColor: '#DC3545',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cofigTablasServices.eliminarTipoActivo(item.id).subscribe({
+          next: () => {
+            this.tipodeactivo = this.tipodeactivo.filter(t => t.id !== item.id);
+            Swal.fire('Eliminado', 'El tipo de activo ha sido eliminado.', 'success');
+          },
+          error: () => {
+            Swal.fire('Error', 'No se pudo eliminar el tipo de activo.', 'error');
+          }
+        });
+      }
+    });
+  }
+  
+  //Agregar nuevo dato en proceso
+  agregarNuevoProceso() {
+    if (this.nuevoProceso.trim()) {
+      const data = { nombre: this.nuevoProceso };
+      this.cofigTablasServices.agregarProceso(data).subscribe({
+        next: (respuesta) => {
+          console.log('Proceso agregado:', respuesta);
+          this.proceso.push(respuesta); // actualizar la tabla
+          this.nuevoProceso = ''; // limpiar el input
+        },
+        error: (error) => {
+          console.error('Error al agregar tipo de activo:', error);
+        }
+      });
+    }
+  }
+
+  //Eliminar datos de tipo de activos 
+  eliminarProceso(item: any) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `Se eliminará el proceso "${item.nombre}"`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      confirmButtonColor: '#DC3545',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cofigTablasServices.eliminarProceso(item.id).subscribe({
+          next: () => {
+            this.proceso = this.proceso.filter(t => t.id !== item.id);
+            Swal.fire('Eliminado', 'El proceso ha sido eliminado.', 'success');
+          },
+          error: () => {
+            Swal.fire('Error', 'No se pudo eliminar el proceso.', 'error');
+          }
+        });
+      }
+    });
+  }
+
+  //Agregar nuevo dato en Responsable/Custodio
+  agregarNuevoCustodio() {
+    if (this.nuevoCustodio.trim()) {
+      const data = { nombre: this.nuevoCustodio };
+      this.cofigTablasServices.agregarResponsable(data).subscribe({
+        next: (respuesta) => {
+          console.log('Proceso agregado:', respuesta);
+          this.custodio.push(respuesta); // actualizar la tabla
+          this.nuevoCustodio = ''; // limpiar el input
+        },
+        error: (error) => {
+          console.error('Error al agregar tipo de activo:', error);
+        }
+      });
+    }
+  }
+
+  //Eliminar datos de tipo de activos 
+  eliminarCustodio(item: any) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `Se eliminará el proceso "${item.nombre}"`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      confirmButtonColor: '#DC3545',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cofigTablasServices.eliminarResponsable(item.id).subscribe({
+          next: () => {
+            this.custodio = this.custodio.filter(t => t.id !== item.id);
+            Swal.fire('Eliminado', 'El proceso ha sido eliminado.', 'success');
+          },
+          error: () => {
+            Swal.fire('Error', 'No se pudo eliminar el proceso.', 'error');
+          }
+        });
+      }
+    });
+  }
+
+  //Agregar nuevo dato en Datos Personales
+  agregarNuevoDatosPersonales() {
+    if (this.nuevoDatosPersonales.trim()) {
+      const data = { nombre: this.nuevoDatosPersonales };
+      this.cofigTablasServices.agregarDatosPersonales(data).subscribe({
+        next: (respuesta) => {
+          console.log('Proceso agregado:', respuesta);
+          this.datospersonales.push(respuesta); // actualizar la tabla
+          this.nuevoDatosPersonales = ''; // limpiar el input
+        },
+        error: (error) => {
+          console.error('Error al agregar tipo de activo:', error);
+        }
+      });
+    }
+  }
+
+  //Eliminar datos de datos personales
+  eliminarDatosPersonales(item: any) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `Se eliminará el proceso "${item.nombre}"`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      confirmButtonColor: '#DC3545',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cofigTablasServices.eliminarDatosPersonales(item.id).subscribe({
+          next: () => {
+            this.datospersonales = this.datospersonales.filter(t => t.id !== item.id);
+            Swal.fire('Eliminado', 'El proceso ha sido eliminado.', 'success');
+          },
+          error: () => {
+            Swal.fire('Error', 'No se pudo eliminar el proceso.', 'error');
+          }
+        });
+      }
+    });
+  }
+
+  //Agregar nuevo dato en Dueño de Activo
+  agregarNuevoDuenoActivo() {
+    if (this.nuevoDuenoActivo.trim()) {
+      const data = { nombre: this.nuevoDuenoActivo };
+      this.cofigTablasServices.agregarDuenoActivo(data).subscribe({
+        next: (respuesta) => {
+          console.log('Proceso agregado:', respuesta);
+          this.duenodeactivo.push(respuesta); // actualizar la tabla
+          this.nuevoDuenoActivo = ''; // limpiar el input
+        },
+        error: (error) => {
+          console.error('Error al agregar tipo de activo:', error);
+        }
+      });
+    }
+  }
+
+  //Eliminar datos de dueño activo
+  eliminarDuenoActivo(item: any) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `Se eliminará el proceso "${item.nombre}"`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      confirmButtonColor: '#DC3545',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cofigTablasServices.eliminarDuenoActivo(item.id).subscribe({
+          next: () => {
+            this.duenodeactivo = this.duenodeactivo.filter(t => t.id !== item.id);
+            Swal.fire('Eliminado', 'El proceso ha sido eliminado.', 'success');
+          },
+          error: () => {
+            Swal.fire('Error', 'No se pudo eliminar el proceso.', 'error');
+          }
+        });
+      }
+    });
+  }
 }
--- front-end/src/app/services/config-tablas.service.ts
+++ front-end/src/app/services/config-tablas.service.ts
@@ -1,9 +1,64 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigTablasService {
 
-  constructor() { }
+  private baseUrl = 'http://127.0.0.1:8000/api/'
+
+  constructor(private htttp: HttpClient) { }
+
+
+  //Agregar nuevo campo tabla tipo de activos
+  agregarTipoActivo(data: any): Observable<any>{
+    return this.htttp.post<any>(`${this.baseUrl}tipodeactivoCustom/`, data)
+  }
+
+  //Eliminar o desactivar campo de tipos de activos 
+  eliminarTipoActivo(id: number): Observable<any>{
+    return this.htttp.delete(`${this.baseUrl}tipodeactivoCustom/${id}/`)
+  }
+
+  //Agregar nuevo campo tabla de proceso
+  agregarProceso(data: any): Observable<any>{
+    return this.htttp.post<any>(`${this.baseUrl}procesoCustom/`, data)
+  }
+
+  //Eliminar o desactivar campo de proceso 
+  eliminarProceso(id: number): Observable<any>{
+    return this.htttp.delete(`${this.baseUrl}procesoCustom/${id}/`)
+  }
+
+  //Agregar tabla responsable/Custodio
+  agregarResponsable(data: any): Observable<any>{
+    return this.htttp.post<any>(`${this.baseUrl}custodioCustom/`, data)
+  }
+
+  //Eliminar tabla responsable/Custodio
+  eliminarResponsable(id: number): Observable<any>{
+    return this.htttp.delete(`${this.baseUrl}custodioCustom/${id}/`)
+  }
+
+  //Agregar datos tabla datos personales
+  agregarDatosPersonales(data: any): Observable<any>{
+    return this.htttp.post<any>(`${this.baseUrl}datospersonalesCustom/`, data)
+  }
+
+  //Eliminar datos tabla datos personales
+  eliminarDatosPersonales(id: number): Observable<any>{
+    return this.htttp.delete(`${this.baseUrl}datospersonalesCustom/${id}/`)
+  }
+
+  //Agregar datos dueño activo
+  agregarDuenoActivo(data: any): Observable<any>{
+    return this.htttp.post<any>(`${this.baseUrl}duenodeactivoCustom/`, data)
+  }
+
+  //Eliminar datos dueño activo
+  eliminarDuenoActivo(id: number): Observable<any>{
+    return this.htttp.delete(`${this.baseUrl}duenodeactivoCustom/${id}/`)
+  }
 }
