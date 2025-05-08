0798cf93b469be161456f7cf9b38b132c005908c
CarmenHerreraB <carmen.herrera@globalteksecurity.com>
2025-05-08 12:17:54 -0500

    .

--- backend/database/models/criticidad.py
+++ backend/database/models/criticidad.py
@@ -5,7 +5,7 @@ class Criticidad(models.Model):
     estado = models.CharField(max_length=255)
     valor_min = models.IntegerField()
     valor_max=models.IntegerField(default=10)
-    color= models.CharField(max_length=9, default='#FFFFFF')
+    color= models.CharField(max_length=9, default='#ffffff', null= True)
     valor_minActualizado = models.IntegerField(null=True)
     valor_maxActualizado=models.IntegerField(null=True)
     colorActualizado= models.CharField(max_length=9, null=True)
